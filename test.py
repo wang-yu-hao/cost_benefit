dd60386e5b60fc16151e391d4b1fd7a895271900
Yuhao Wang <yuhao.wang@ndcn.ox.ac.uk>
2023-02-15 10:06:10 +0000

    Sync. No significant update.

--- test.py
+++ test.py
@@ -6,3 +6,4 @@ import import_beh_data as bi
 session = bi.Session('../../../OneDrive - Nexus365/Data/Basal_Ganglia_Cost_Benefit/Data/ThreeChoice_Task/JC06_ThreeChoice_A2a_Drd1/Photometry/Behaviour/A7.5b-2022-11-12-140835.txt')
 
 session.events_and_print
+
